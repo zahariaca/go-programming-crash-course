637a38e454ece4e6d95cbbf0f8a204bf13a9b34c
zahariaca <zaharia.c.alexandru@gmail.com>
2023-03-31 19:00:11 +0300

    Section 1 Structure of Go Program

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	fmt.Print("This is some text")
+	fmt.Print("This is some  more text")
+	sayHelloWorld("")
+	sayHelloWorld("Hello World, again!")
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
