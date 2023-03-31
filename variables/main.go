2c7e107332a3e00a7a0c97e20605c8fb9a5bb27f
zahariaca <zaharia.c.alexandru@gmail.com>
2023-03-31 19:44:36 +0300

    Adds answer reply

--- variables/main.go
+++ variables/main.go
@@ -9,19 +9,19 @@ import (
 const prompt = "and press ENTER when ready"
 
 func main() {
-	
+
 	// one way - declare, then assign (two steps)
 	var firstNumber int
 	firstNumber = 2
-	
+
 	// another way, declare type and name and assign value
 	var secondNumber = 5
-	
+
 	// one step variable: declare name, assign value and let go figure out the type
 	subtraction := 7
-	
+
 	var answer int
-	
+
 	reader := bufio.NewReader(os.Stdin)
 
 	// display a welcome/instruction
@@ -47,5 +47,9 @@ func main() {
 	fmt.Println("Now subtract", subtraction, prompt)
 
 	// give them the answer
+
+	answer = firstNumber*secondNumber - subtraction
+
+	fmt.Println("The answer is", answer)
 
 }
