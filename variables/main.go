3a3f10494924601bd2875e308eee6e2c7cba3cdf
zahariaca <zaharia.c.alexandru@gmail.com>
2023-03-31 19:37:09 +0300

    Section 02 Variables

---
+++ variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("test")
+
+	// one way - declare, then assign (two steps)
+	var firstNumber int
+	firstNumber = 2
+
+	// another way, declare type and name and assign value
+	var secondNumber = 5
+
+	// one step variable: declare name, assign value and let go figure out the type
+	subtraction := 7
+	
+}
