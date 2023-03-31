f63d7d5278b7bc9e5d2ede34ff446bb25002cde8
zahariaca <zaharia.c.alexandru@gmail.com>
2023-03-31 20:23:35 +0300

    Scope Challenge solution

--- scope/main.go
+++ scope/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 var packageLevelVariable = "package level value"
+var myVar = "myVarValue"
 
 func main() {
 	var one = "One"
@@ -21,6 +22,25 @@ func main() {
 	fmt.Println("From packageone:", newString)
 
 	packageone.Exported()
+
+	// variables
+
+	// declare a package level variable for main
+	// package named myVar
+
+	// declare a block variable for the main function
+	// called blockVar
+	var blockVar = "blockVarValue"
+
+	// declare a package level variable in the packageone
+	// package named PackageVar
+
+	// create an exported function in packageone called PrintMe
+
+	// in the main function, print out hte values of myVar,
+	// blockVar, and PackageVar on oneline using the PrintMe
+	// function in packageone
+	packageone.PrintMe(myVar, blockVar, packageone.PackageVar)
 }
 
 func myFunc() {
