9bcbadc90c4fd568376837bd1ef600286a7b1d71
zahariaca <zaharia.c.alexandru@gmail.com>
2023-03-31 19:43:03 +0300

    Adds all necessary prompts

--- variables/main.go
+++ variables/main.go
@@ -1,18 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER when ready"
 
 func main() {
-	fmt.Println("test")
-
+	
 	// one way - declare, then assign (two steps)
 	var firstNumber int
 	firstNumber = 2
-
+	
 	// another way, declare type and name and assign value
 	var secondNumber = 5
-
+	
 	// one step variable: declare name, assign value and let go figure out the type
 	subtraction := 7
 	
+	var answer int
+	
+	reader := bufio.NewReader(os.Stdin)
+
+	// display a welcome/instruction
+
+	fmt.Println("Guess the Number Game")
+	fmt.Println("---------------------")
+	fmt.Println()
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	//take them through the games
+
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+
+	// give them the answer
+
 }
